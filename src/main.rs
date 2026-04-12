346b38cd710a96958ee6ff5729ecf8734b0ce697
JakeTurnick <jaketurnick@gmail.com>
2026-04-12 09:29:49 -0400

    WASD controls for menu, Space to select MenuItem & Color

--- src/main.rs
+++ src/main.rs
@@ -173,14 +173,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, simon: &mut Simon) -> io::Res
                                     // GameState::new() // reset the game
                                     continue;
                                 }
-                                if let Some(selection) = simon.app_state.menu_list.state.selected() {
-                                    let selected_scene = simon.app_state.menu_list.items[selection].scene;
-                                    
-                                    simon.app_state.change_scene(selected_scene);
-                                    if simon.app_state.is_paused {
-                                        simon.app_state.is_paused = false;
-                                    }
-                                } // else { println!("no item");}
+                                select_menu_item(simon);
                             }
                             KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right
                                 if simon.app_state.current_scene == Scene::Game && !simon.app_state.is_paused => {
@@ -191,13 +184,25 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, simon: &mut Simon) -> io::Res
                             KeyCode::Char(c) =>  {
                                 if !simon.app_state.enable_text_entry {
                                     /* char commands */
-                                    match c {
-                                        'q' => { simon.app_state.is_paused = !simon.app_state.is_paused }
-                                        'w' => { simon.handle_keyboard_color_selection(key.code); }
-                                        'a' => { simon.handle_keyboard_color_selection(key.code); }
-                                        's' => { simon.handle_keyboard_color_selection(key.code); }
-                                        'd' => { simon.handle_keyboard_color_selection(key.code); }
-                                        _ => {}
+                                    if simon.app_state.current_scene != Scene::Game {
+                                        if c.is_whitespace() { select_menu_item(simon); }
+                                        match c {
+                                            'w' | 'a' => { simon.select_previous_list_item(); }
+                                            's' | 'd' => { simon.select_next_list_item(); }
+                                            _ => {}
+                                        }
+                                    } else {
+                                        if c.is_whitespace() { 
+                                            if let Some(color) = simon.game_state.hovered_color {
+                                                simon.handle_player_guess(color);
+                                            }
+                                        }
+                                        match c {
+                                            'w' | 'a' | 's' | 'd' => { 
+                                                simon.handle_keyboard_color_selection(key.code); 
+                                            }
+                                            _ => {}
+                                        }
                                     }
                                 } else {
                                     /* raw text input */
@@ -214,15 +219,8 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, simon: &mut Simon) -> io::Res
                         simon.game_state.mouse_pos = (mouse.column, mouse.row);
 
                         if mouse.kind == event::MouseEventKind::Down(MouseButton::Left) {
-                            let pos = simon.game_state.mouse_pos.into();
-                            let clicked_color = simon.game_state.clickables.iter().rev()
-                                .find(|(_, r)| r.contains(pos))
-                                .map(|(color, _)| *color);
-
-                            if let Some(color) = clicked_color {                                
+                            if let Some(color) = simon.game_state.hovered_color {
                                 simon.handle_player_guess(color);
-                            } else {
-                                // simon.debug_msg = format!("MISS! No color at {:?}", pos);
                             }
                         }
                         
@@ -251,6 +249,17 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, simon: &mut Simon) -> io::Res
 
                 let _ = terminal.draw(|f| ui(f, simon));
             }
+        }
+    }
+}
+
+fn select_menu_item(simon: &mut Simon) {
+    if let Some(selection) = simon.app_state.menu_list.state.selected() {
+        let selected_scene = simon.app_state.menu_list.items[selection].scene;
+        
+        simon.app_state.change_scene(selected_scene);
+        if simon.app_state.is_paused {
+            simon.app_state.is_paused = false;
         }
     }
 }
